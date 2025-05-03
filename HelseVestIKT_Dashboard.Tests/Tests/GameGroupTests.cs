71fda9aa205cd7aaed8bf230eaede6a25fb163c7
h669791 <669791@stud.hvl.no>
2025-05-03 19:59:01 +0200

    - Lagd noen testklasser for å teste funksjonalitet i prosjektet

---
+++ HelseVestIKT_Dashboard.Tests/Tests/GameGroupTests.cs
@@ -0,0 +1,51 @@
+﻿using NUnit.Framework;
+using System.Threading;
+using HelseVestIKT_Dashboard.Models;   // ditt hoved-namespace
+
+namespace HelseVestIKT_Dashboard.Tests
+{
+	/// <summary>
+	/// Tester funksjonaliteten i GameGroup-klassen, spesielt HasGame-metoden
+	/// for å sikre at den oppdager når spill er lagt til eller ikke.
+	/// </summary>
+
+	[TestFixture, Apartment(ApartmentState.STA)]
+	public class GameGroupTests
+	{
+
+		/// <summary>
+		/// Skal returnere true når et spill er lagt til i gruppen.
+		/// </summary>
+		[Test]
+		public void HasGame_ReturnsTrue_WhenGameIsAdded()
+		{
+			// Arrange
+			var game = new Game { AppID = "1", Title = "TestGame" };
+			var group = new GameGroup { GroupName = "MinGruppe" };
+			group.Games.Add(game);
+
+			// Act
+			bool finnes = group.HasGame(game);
+
+			// Assert
+			Assert.IsTrue(finnes, "HasGame skal returnere true når spillet er lagt til.");
+		}
+
+		/// <summary>
+		/// Skal returnere false når gruppen er tom og ikke inneholder spillet.
+		/// </summary>
+		[Test]
+		public void HasGame_ReturnsFalse_WhenGroupIsEmpty()
+		{
+			// Arrange
+			var group = new GameGroup { GroupName = "TomGruppe" };
+			var game = new Game { AppID = "2", Title = "AnnetSpill" };
+
+			// Act
+			bool finnes = group.HasGame(game);
+
+			// Assert
+			Assert.IsFalse(finnes, "HasGame skal returnere false når gruppen er tom.");
+		}
+	}
+}
---
+++ HelseVestIKT_Dashboard.Tests/Tests/SpillFilterTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using System.ComponentModel;
+using HelseVestIKT_Dashboard.Models;
+
+namespace HelseVestIKT_Dashboard.Tests
+{
+
+	/// <summary>
+	/// Tester SpillFilter-klassen for å verifisere at PropertyChanged-eventet
+	/// skytes når navnet endres og at riktig PropertyName rapporteres.
+	/// </summary>
+	[TestFixture]
+	public class SpillFilterTests
+	{
+
+		/// <summary>
+		/// Skal utløse PropertyChanged når Name-verdien endres.
+		/// </summary>
+		[Test]
+		public void Name_PropertyChanged_EventFires()
+		{
+			// Arrange
+			var filter = new SpillFilter();
+			string changedProperty = null!;
+			filter.PropertyChanged += (s, e) => changedProperty = e.PropertyName;
+
+			// Act
+			filter.Name = "NyttNavn";
+
+			// Assert
+			Assert.AreEqual(nameof(filter.Name), changedProperty, "PropertyChanged skal varsle når Name endres.");
+			Assert.AreEqual("NyttNavn", filter.Name, "Name skal ha den nye verdien etter endring.");
+		}
+	}
+}
